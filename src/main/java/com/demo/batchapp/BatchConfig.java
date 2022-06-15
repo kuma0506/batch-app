ece46e8ae266dfd6aa6fc450e5082de82fffab0e
takuya.kumada <looow.tk@gmail.com>
2022-06-15 17:40:35 +0900

    処理実装

---
+++ src/main/java/com/demo/batchapp/BatchConfig.java
@@ -0,0 +1,45 @@
+package com.demo.batchapp;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.demo.batchapp.task.Task1;
+import com.demo.batchapp.task.Task2;
+
+@EnableBatchProcessing // バッチの基本機能提供
+@Configuration // Beanの生成(1つ以上のBeanが必要)
+public class BatchConfig {
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Task1())
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(new Task2())
+                .build();
+    }
+
+    @Bean
+    public Job job(Step step1, Step step2, JobBuilderFactory jobBuilderFactory) {
+        return jobBuilderFactory.get("job")
+                .incrementer(new RunIdIncrementer())
+                .start(step1)
+                .next(step2)
+                .build();
+    }
+
+}
